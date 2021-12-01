14f9564e06a854e6de5099171ccbc6973da6cbe0
Michał Nejman <michal.nejman@spantium.com>
2021-12-01 18:04:41 +0100

    did some work on structs

---
+++ structures/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+    public struct numbers
+    {
+       public int x, y;
+        public numbers(int num1,int num2)
+        {
+            x = num1;
+            y = num2;
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+            numbers numbers1 = new numbers();
+            numbers numbers2 = new numbers(2,2);
+
+            Console.WriteLine("First set of numbers: x={0}, y={1}",numbers1.x,numbers1.y);
+            Console.WriteLine("\nSecond set of numbers: x={0}, y={1}", numbers2.x, numbers2.y);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ structures/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+        struct ksiązka
+        {
+            public string tytul;
+            public string autor;
+            public int rokwydania;
+        }
+    //Write a program in C# Sharp to insert the information of two books.
+    class Program
+    {
+       
+
+        static void Main()
+        {
+            int numerksiazek = 1000;
+            ksiązka[] ksiazki = new ksiązka[numerksiazek];
+            int x, y, z = 1, a = 1;
+
+            for(x=0;x<=z;x++)
+            {
+                Console.WriteLine("Ksiazka numer {0} :", a);
+                Console.WriteLine("Wprowadz tytuł ksiazki:");
+                ksiazki[x].tytul =Console.ReadLine();
+
+                Console.WriteLine("Wprowadz autora ksiazki:");
+                ksiazki[x].autor = Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine("Wprowadz rok wydania ksiazki:");
+                ksiazki[x].rokwydania = Console.Read();
+                a++;
+                Console.WriteLine();
+            }
+
+            for(y=0;y<=z;y++)
+            {
+                Console.WriteLine("{0}. Tytuł={1}, Autor={2},Rok wydania={3}\n", x + 1, ksiazki[x].tytul, ksiazki[x].autor, ksiazki[x].rokwydania);
+                Console.WriteLine();
+                Console.Read();
+            }
+           
+        }
+    }
+}
