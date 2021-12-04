7e44e01dc7838e3abc38ad4b444b5da00efdb356
Michał Nejman <michal.nejman@spantium.com>
2021-12-04 13:19:12 +0100

    added some functions exersises

--- functions/ConsoleApp1/ConsoleApp1/Program.cs
+++ functions/ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,11 +2,33 @@
 
 namespace ConsoleApp1
 {
+    //Write a program in C# Sharp to calculate the sum of elements in an array.
+
+
     class Program
     {
-        static void Main(string[] args)
+        public static int Sum(int[] array)
         {
-            Console.WriteLine("Hello World!");
+            int suma = 0;
+            for(int i=0;i<array.Length;i++)
+            {
+                suma += array[i];
+               
+            }
+            return suma;
+        }
+        static void Main()
+        {
+            int[] array = new int[5];
+            for(int j=0;j<5;j++)
+            {
+                Console.WriteLine("element-{0}:",j+1);
+                array[j] = Convert.ToInt32(Console.ReadLine());
+
+
+            }
+            Console.WriteLine("Suma tych 5 liczb to:{0}", Sum(array));
+            Console.Read();
         }
     }
 }
