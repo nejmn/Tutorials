1fd5d8a16905920bbfb0ffa720381d687f7d4749
Michał Nejman <michal.nejman@spantium.com>
2021-12-07 19:56:03 +0100

    Started working on file handling

---
+++ File Handling/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace ConsoleApp1
+{
+    //Write a program in C# Sharp to create a blank file in the disk newly. 
+    class Program
+    {
+        static void Main()
+        {
+
+            string file = @"test.txt";
+
+
+            try
+            {
+
+                using (FileStream fileStream = File.Create(file))
+                {
+                Console.WriteLine("Został stworzony plik o nazwie{0}",file);
+                }
+            }
+            catch(Exception exeption)
+            {
+                Console.WriteLine(exeption.ToString());
+            }
+            
+        }
+    }
+}
---
+++ File Handling/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+namespace ConsoleApp2
+{
+    //Write a program in C# Sharp to read a specific line from a file.
+    class Program
+    {
+        static void Main()
+        {
+            string file = "filename.txt";
+            string[] arrayOfLines;
+            int x, i, z;
+
+
+
+            if(File.Exists(file))
+            {
+                File.Delete(file);
+            }
+
+
+
+            Console.WriteLine("Podaj numer linijek ktore mamy wpisac do pliku:\n");
+
+            x = Convert.ToInt32(Console.ReadLine());
+            arrayOfLines = new string[x];
+      
+            for(i=0;i<x;i++)
+            {
+                Console.Write("Podaj tresc {0} linijki\n", i+1);
+                arrayOfLines[i] = Console.ReadLine();
+
+            }
+
+            System.IO.File.WriteAllLines(file, arrayOfLines);
+
+            Console.Write("Podaj numer linijki ktorą chcesz wyswietlic\n");
+            z = Convert.ToInt32(Console.ReadLine());
+            if(z>=1&&z<=x)
+            {
+                if(File.Exists(file))
+                {
+                    string[] lines = File.ReadAllLines(file);
+                    Console.WriteLine("{0}", lines[z - 1]);
+
+                }
+            }
+            else
+            {
+                Console.Write("nieprawidłowy numer linijki ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
