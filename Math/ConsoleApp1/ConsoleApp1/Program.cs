dc35d61cd00f213400db225d195b854bc383bea7
Michał Nejman <michal.nejman@spantium.com>
2021-12-09 17:52:52 +0100

    Added some Math exersises

---
+++ Math/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+namespace ConsoleApp1
+{
+    //Write a C# Sharp program to determine the sign of a single value and display it to the console.
+    class Program
+    {
+        static void Main()
+        {
+            int integer=-789;
+            float float1 = 23.34f;
+            byte byte1 = 0;
+            long long1 = -4;
+            double double1=2.2;
+            Console.WriteLine("{0} {1}\n",integer,Check(Math.Sign(integer)));
+            Console.WriteLine("{0} {1}\n", float1, Check(Math.Sign(float1)));
+            Console.WriteLine("{0} {1}\n", byte1, Check(Math.Sign(byte1)));
+            Console.WriteLine("{0} {1}\n", long1, Check(Math.Sign(long1)));
+            Console.WriteLine("{0} {1}\n", double1, Check(Math.Sign(double1)));
+            Console.ReadLine();
+        }
+
+
+
+        public static string Check(int value)
+        {
+            if (value == 0) return "jest równe zero";
+            else if (value < 0) return "jest mniejsze od zera";
+            else return "jest wieksze od zera";
+        }
+
+    }
+}
---
+++ Math/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    //Write a C# Sharp program to reverse the binary representation of an given integer and convert the reversed binary number into an integer.
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            Console.WriteLine("Liczba:"+x);
+            Console.WriteLine("Liczba ktora powstanie po odwroceniu liczby binarnej:" +ReverseBinary(x));
+
+            int y = 100;
+            Console.WriteLine("Liczba:" + y);
+            Console.WriteLine("Liczba ktora powstanie po odwroceniu liczby binarnej:" + ReverseBinary(y));
+
+            int z = 0;
+            Console.WriteLine("Liczba:" + z);
+            Console.WriteLine("Liczba ktora powstanie po odwroceniu liczby binarnej:" + ReverseBinary(z));
+
+            int b = 1534;
+            Console.WriteLine("Liczba:" +b);
+            Console.WriteLine("Liczba ktora powstanie po odwroceniu liczby binarnej:" + ReverseBinary(b));
+
+            int a = 254;
+            Console.WriteLine("Liczba:" + a);
+            Console.WriteLine("Liczba ktora powstanie po odwroceniu liczby binarnej:" + ReverseBinary(a));
+            Console.ReadLine();
+        }
+
+
+        public static int ReverseBinary(int number)
+        {
+            return (Convert.ToInt32(new string(Convert.ToString(number, 2).Reverse().ToArray()), 2));
+        }
+    }
+}
